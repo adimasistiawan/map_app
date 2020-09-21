1504bb226a2a541aa9ee302326ac13f9e7cd0e81
adimasputra <adimasistiawan01@gmail.com>
2020-09-21 10:20:53 +0800

    polyline tidak muncul
    polyline tidak muncul

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_map_polyline/google_map_polyline.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
+import 'package:intl/intl.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,14 +25,70 @@ class Gmap extends StatefulWidget {
 
 class _GmapState extends State<Gmap> {
   // Map<MarkerId, Marker> dataMarker = <MarkerId, Marker>{};
-  List<Marker> dataMarker = <Marker>[];
-  GoogleMapController _mapcontroller;
-  Completer<GoogleMapController> _controller = Completer();
-
-  String test;
   String _name;
   final GlobalKey<FormState> formkey = GlobalKey<FormState>();
-  var markers = [];
+  List<Marker> dataMarker = <Marker>[];
+  List<PolylineWayPoint> point = [];
+  GoogleMapController _mapcontroller;
+  Completer<GoogleMapController> _controller = Completer();
+  bool polyactive = false;
+  List<LatLng> ltlg = List();
+  List<LatLng> routeCoords = [];
+  PolylinePoints polylinePoints = PolylinePoints();
+  Map<PolylineId, Polyline> polylines = {};
+  String googleAPiKey = "AIzaSyDtAHkHxL4Sv5GvJranFwSJwrz33aRoTpc";
+  GoogleMapPolyline googleMapPolyline =
+      new GoogleMapPolyline(apiKey: "AIzaSyDrJN7HOsyZLw6_PucyKTx7LTaMKjEenvg");
+  PolylineResult result;
+  List<LatLng> result2;
+  // LatLng t1 = LatLng(-8.640705240362454, 115.20013488829136);
+  // LatLng t2 = LatLng(-8.6673892, 115.1960675);
+  // LatLng t3 = LatLng(-8.66123, 115.1954642);
+  // LatLng t4 = LatLng(-8.65706064452849, 115.2157885953784);
+
+  @override
+  void initState() {
+    super.initState();
+    drawRoute();
+    setState(() {});
+  }
+
+  drawRoute() async {
+    // ------- Flutter map polyline--------------
+    result = await polylinePoints.getRouteBetweenCoordinates(googleAPiKey,
+        PointLatLng(-8.66123, 115.1954642), PointLatLng(-8.65706, 115.2157885),
+        travelMode: TravelMode.driving);
+    print(result);
+    if (result.points.isNotEmpty) {
+      // jika result tidak kosong akan muncul draw route
+      result.points.forEach((PointLatLng point) {
+        routeCoords.add(LatLng(point.latitude, point.longitude));
+      });
+      PolylineId id = PolylineId("poly");
+      Polyline polyline =
+          Polyline(polylineId: id, color: Colors.red, points: routeCoords);
+      polylines[id] = polyline;
+    } else {
+      // jika result kosong akan membuat polyline biasa
+      PolylineId id = PolylineId("poly");
+      Polyline polyline = Polyline(polylineId: id, color: Colors.red, points: [
+        LatLng(-8.66123, 115.1954642),
+        LatLng(-8.65706064452849, 115.2157885953784)
+      ]);
+      polylines[id] = polyline;
+    }
+
+    // ------- Google map polyline-----------
+    // result2 = await googleMapPolyline.getCoordinatesWithLocation(
+    //     origin: LatLng(-8.65706064452849, 115.2157885953784),
+    //     destination: LatLng(-8.66123, 115.1954642),
+    //     mode: RouteMode.driving);
+    // print(result2);
+    // PolylineId id = PolylineId("poly");
+    // Polyline polyline =
+    //     Polyline(polylineId: id, color: Colors.red, points: result2);
+    // polylines[id] = polyline;
+  }
 
   void popup(BuildContext context, latitude, longitude) {
     final textcontroller = TextEditingController();
@@ -109,6 +168,7 @@ class _GmapState extends State<Gmap> {
                 onPressed: () {
                   updateLocation(id);
                   editcontroller.clear();
+
                   _mapcontroller.hideMarkerInfoWindow(MarkerId(id));
                 },
               ),
@@ -156,91 +216,158 @@ class _GmapState extends State<Gmap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Map App'),
-        ),
-        body: Stack(
-          children: [
-            StreamBuilder(
-              stream: Firestore.instance.collection('markers').snapshots(),
-              builder: (context, snapshot) {
-                dataMarker.clear();
-                if (!snapshot.hasData) {
-                  return Text("Loading..");
-                }
-                if (snapshot.data.documents.isNotEmpty) {
-                  for (var i = 0; i < snapshot.data.documents.length; i++) {
-                    markers.add(snapshot.data.documents[i].data);
-                    dataMarker.add(Marker(
-                        markerId:
-                            MarkerId((snapshot.data.documents[i].documentID)),
-                        onTap: () {
-                          showModalBottomSheet(
-                              barrierColor: Colors.white.withOpacity(0),
-                              context: context,
-                              builder: (context) {
-                                return Container(
-                                  height: 120,
-                                  child: Container(
-                                    child: Column(
-                                      children: [
-                                        ListTile(
-                                          leading: Icon(Icons.edit),
-                                          title: Text("Edit"),
-                                          onTap: () {
-                                            Navigator.pop(context);
-                                            edit(
-                                                context,
-                                                snapshot.data.documents[i]
-                                                    .data['location'],
-                                                (snapshot.data.documents[i]
-                                                    .documentID));
-                                          },
-                                        ),
-                                        ListTile(
-                                          leading: Icon(Icons.delete),
-                                          title: Text("Delete"),
-                                          onTap: () {
-                                            deleteLocation(snapshot
-                                                .data.documents[i].documentID);
-                                            Navigator.pop(context);
-                                          },
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              });
-                        },
-                        position: LatLng(
-                            (snapshot
-                                .data.documents[i].data['coordinate'].latitude),
-                            (snapshot.data.documents[i].data['coordinate']
-                                .longitude)),
-                        infoWindow: InfoWindow(
-                          title: (snapshot.data.documents[i].data['location']),
-                        )));
+      appBar: AppBar(
+        title: Text('Map App'),
+      ),
+      body: Stack(
+        children: [
+          StreamBuilder(
+            stream: Firestore.instance
+                .collection('markers')
+                .orderBy('date', descending: false)
+                .snapshots(),
+            builder: (context, snapshot) {
+              dataMarker.clear();
+              ltlg.clear();
+              Timestamp time = new Timestamp(2019, 3);
+              if (!snapshot.hasData) {
+                return Text("Loading..");
+              }
+              if (snapshot.data.documents.isNotEmpty) {
+                for (var i = 0; i < snapshot.data.documents.length; i++) {
+                  // ltlg.add(LatLng(
+                  //     snapshot.data.documents[i].data['coordinate'].latitude,
+                  //     snapshot.data.documents[i].data['coordinate'].longitude));
+                  // point.add(PolylineWayPoint(
+                  //     location: (snapshot
+                  //             .data.documents[i].data['coordinate'].latitude
+                  //             .toString() +
+                  //         ", " +
+                  //         snapshot
+                  //             .data.documents[i].data['coordinate'].longitude
+                  //             .toString())));
+
+                  if (snapshot.data.documents[i].data['date'] != null) {
+                    time = snapshot.data.documents[i].data['date'];
                   }
+
+                  dataMarker.add(Marker(
+                      markerId:
+                          MarkerId((snapshot.data.documents[i].documentID)),
+                      onTap: () {
+                        showModalBottomSheet(
+                            barrierColor: Colors.white.withOpacity(0),
+                            context: context,
+                            builder: (context) {
+                              return Container(
+                                height: 120,
+                                child: Container(
+                                  child: Column(
+                                    children: [
+                                      ListTile(
+                                        leading: Icon(Icons.edit),
+                                        title: Text("Edit"),
+                                        onTap: () {
+                                          Navigator.pop(context);
+                                          edit(
+                                              context,
+                                              snapshot.data.documents[i]
+                                                  .data['location'],
+                                              (snapshot.data.documents[i]
+                                                  .documentID));
+                                        },
+                                      ),
+                                      ListTile(
+                                        leading: Icon(Icons.delete),
+                                        title: Text("Delete"),
+                                        onTap: () {
+                                          deleteLocation(snapshot
+                                              .data.documents[i].documentID);
+                                          Navigator.pop(context);
+                                        },
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            });
+                      },
+                      position: LatLng(
+                          (snapshot
+                              .data.documents[i].data['coordinate'].latitude),
+                          (snapshot
+                              .data.documents[i].data['coordinate'].longitude)),
+                      infoWindow: InfoWindow(
+                        title: (snapshot.data.documents[i].data['location'] +
+                            " (" +
+                            DateFormat('yyyy-MM-dd – kk:mm')
+                                .format(time.toDate())
+                                .toString() +
+                            ")"),
+                      )));
                 }
-                return new GoogleMap(
-                  onMapCreated: (GoogleMapController controller) {
-                    setState(() {
-                      _mapcontroller = controller;
-                    });
-                  },
-                  onTap: (coordinate) {
-                    popup(context, coordinate.latitude, coordinate.longitude);
-                  },
-                  mapType: MapType.normal,
-                  initialCameraPosition: CameraPosition(
-                    target: LatLng(-8.650000, 115.216667),
-                    zoom: 14.0,
+                // var j = snapshot.data.documents.length - 1;
+                // PointLatLng a = PointLatLng(
+                //     snapshot.data.documents[0].data['coordinate'].latitude,
+                //     snapshot.data.documents[0].data['coordinate'].longitude);
+                // PointLatLng b = PointLatLng(
+                //     snapshot.data.documents[j].data['coordinate'].latitude,
+                //     snapshot.data.documents[j].data['coordinate'].longitude);
+              }
+
+              return new GoogleMap(
+                onMapCreated: (GoogleMapController controller) {
+                  setState(() {
+                    _mapcontroller = controller;
+                  });
+                },
+                onTap: (coordinate) {
+                  popup(context, coordinate.latitude, coordinate.longitude);
+                },
+                polylines: Set<Polyline>.of(polylines.values),
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(-8.650000, 115.216667),
+                  zoom: 14.0,
+                ),
+                markers: Set<Marker>.of(dataMarker),
+              );
+            },
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton:
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: <Widget>[
+                Container(
+                  height: 70.0,
+                  width: 70.0,
+                  child: FloatingActionButton(
+                    child: Icon(Icons.trending_up),
+                    onPressed: () {
+                      if (!polyactive) {
+                        setState(() {
+                          polyactive = true;
+                        });
+                      } else {
+                        setState(() {
+                          polyactive = false;
+                        });
+                      }
+                    },
                   ),
-                  markers: Set<Marker>.of(dataMarker),
-                );
-              },
-            )
+                ),
+              ],
+            ),
           ],
-        ));
+        )
+      ]),
+    );
   }
 }
