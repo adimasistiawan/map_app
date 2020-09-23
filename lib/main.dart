0d464271226e6f5ef340c08b3a0e237c2d619ee7
adimasputra <adimasistiawan01@gmail.com>
2020-09-23 12:23:31 +0800

    polyline fix
    polyline fix

--- lib/main.dart
+++ lib/main.dart
@@ -33,21 +33,17 @@ class _GmapState extends State<Gmap> {
   Completer<GoogleMapController> _controller = Completer();
   bool polyactive = false;
   List<LatLng> ltlg = List();
-  List<LatLng> routeCoords = [];
+
   PolylinePoints polylinePoints = PolylinePoints();
   Map<PolylineId, Polyline> polylines = {};
+  Map<PolylineId, Polyline> emptypolylines = {};
   String googleAPiKey = "AIzaSyAK32XxHYjxr9p9wv_K_wE2-2Xgd563bE4";
   PolylineResult result;
 
-  // LatLng t1 = LatLng(-8.640705240362454, 115.20013488829136);
-  // LatLng t2 = LatLng(-8.6673892, 115.1960675);
-  // LatLng t3 = LatLng(-8.66123, 115.1954642);
-  // LatLng t4 = LatLng(-8.65706064452849, 115.2157885953784);
 
   @override
   void initState() {
     super.initState();
-    getRoute();
     setState(() {});
   }
 
@@ -59,35 +55,24 @@ class _GmapState extends State<Gmap> {
         .getDocuments();
     if (value.documents.isNotEmpty) {
       for (var i = 0; i < value.documents.length; i++) {
-        point.add(PolylineWayPoint(
-            location: value.documents[i].data['coordinate'].latitude
-                    .toString() +
-                ", " +
-                value.documents[i].data['coordinate'].longitude.toString()));
+        if (i + 1 != value.documents.length &&
+            i + 1 <= value.documents.length) {
+          PointLatLng a = PointLatLng(
+              value.documents[i].data['coordinate'].latitude,
+              value.documents[i].data['coordinate'].longitude);
+          PointLatLng b = PointLatLng(
+              value.documents[i + 1].data['coordinate'].latitude,
+              value.documents[i + 1].data['coordinate'].longitude);
+          String c = value.documents[i].data['location'];
+          setRoute(a, b, c);
+          
+        }
       }
-      var j = value.documents.length - 1;
-      PointLatLng a = PointLatLng(
-          value.documents[0].data['coordinate'].latitude,
-          value.documents[0].data['coordinate'].longitude);
-      PointLatLng b = PointLatLng(
-          value.documents[j].data['coordinate'].latitude,
-          value.documents[j].data['coordinate'].longitude);
-      setRoute(a, b, point);
     }
-
-    // ------- Google map polyline-----------
-    // result2 = await googleMapPolyline.getCoordinatesWithLocation(
-    //     origin: LatLng(-8.65706064452849, 115.2157885953784),
-    //     destination: LatLng(-8.66123, 115.1954642),
-    //     mode: RouteMode.driving);
-    // print(result2);
-    // PolylineId id = PolylineId("poly");
-    // Polyline polyline =
-    //     Polyline(polylineId: id, color: Colors.red, points: result2);
-    // polylines[id] = polyline;
   }
 
-  setRoute(origin, destination, wayp) async {
+  setRoute(origin, destination, location) async {
+    List<LatLng> routeCoords = [];
     result = await polylinePoints.getRouteBetweenCoordinates(
         googleAPiKey, origin, destination,
         travelMode: TravelMode.driving);
@@ -97,7 +82,7 @@ class _GmapState extends State<Gmap> {
       result.points.forEach((PointLatLng pointcoor) {
         routeCoords.add(LatLng(pointcoor.latitude, pointcoor.longitude));
       });
-      PolylineId id = PolylineId("poly");
+      PolylineId id = PolylineId(location);
       Polyline polyline =
           Polyline(polylineId: id, color: Colors.red, points: routeCoords);
       setState(() {
@@ -244,25 +229,12 @@ class _GmapState extends State<Gmap> {
                 .snapshots(),
             builder: (context, snapshot) {
               dataMarker.clear();
-              ltlg.clear();
               Timestamp time = new Timestamp(2019, 3);
               if (!snapshot.hasData) {
                 return Text("Loading..");
               }
               if (snapshot.data.documents.isNotEmpty) {
                 for (var i = 0; i < snapshot.data.documents.length; i++) {
-                  // ltlg.add(LatLng(
-                  //     snapshot.data.documents[i].data['coordinate'].latitude,
-                  //     snapshot.data.documents[i].data['coordinate'].longitude));
-                  // point.add(PolylineWayPoint(
-                  //     location: (snapshot
-                  //             .data.documents[i].data['coordinate'].latitude
-                  //             .toString() +
-                  //         ", " +
-                  //         snapshot
-                  //             .data.documents[i].data['coordinate'].longitude
-                  //             .toString())));
-
                   if (snapshot.data.documents[i].data['date'] != null) {
                     time = snapshot.data.documents[i].data['date'];
                   }
@@ -322,13 +294,7 @@ class _GmapState extends State<Gmap> {
                             ")"),
                       )));
                 }
-                // var j = snapshot.data.documents.length - 1;
-                // PointLatLng a = PointLatLng(
-                //     snapshot.data.documents[0].data['coordinate'].latitude,
-                //     snapshot.data.documents[0].data['coordinate'].longitude);
-                // PointLatLng b = PointLatLng(
-                //     snapshot.data.documents[j].data['coordinate'].latitude,
-                //     snapshot.data.documents[j].data['coordinate'].longitude);
+                
 
               }
 
@@ -341,7 +307,9 @@ class _GmapState extends State<Gmap> {
                 onTap: (coordinate) {
                   popup(context, coordinate.latitude, coordinate.longitude);
                 },
-                polylines: Set<Polyline>.of(polylines.values),
+                polylines: polyactive
+                    ? Set<Polyline>.of(polylines.values)
+                    : Set<Polyline>.of(emptypolylines.values),
                 mapType: MapType.normal,
                 initialCameraPosition: CameraPosition(
                   target: LatLng(-8.650000, 115.216667),
@@ -366,12 +334,17 @@ class _GmapState extends State<Gmap> {
                   height: 70.0,
                   width: 70.0,
                   child: FloatingActionButton(
-                    child: Icon(Icons.trending_up),
+                    backgroundColor: polyactive ? Colors.red : Colors.blue,
+                    child: polyactive
+                        ? Icon(Icons.clear)
+                        : Icon(Icons.trending_up),
                     onPressed: () {
                       if (!polyactive) {
                         setState(() {
+                          polylines.clear();
                           polyactive = true;
                         });
+                        getRoute();
                       } else {
                         setState(() {
                           polyactive = false;
